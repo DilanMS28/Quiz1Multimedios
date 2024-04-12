4914a83b38b37df6f8e3078b8b01e45f5a18f86f
Dilan <dilantienda90@gmail.com>
2024-04-11 19:40:15 -0600

    Agregué unos párrafos para saber cual cada ejercicio

--- src/App.js
+++ src/App.js
@@ -9,15 +9,19 @@ import React from "react";
 function App() {
   return (
     <div className="App">
+      <p>Ejercicio 1</p>
       <Likes/>
       <br/>
       <br/>
+      <p>Ejercicio 2</p>
       <Entrada/>
       <br/>
       <br/>
+      <p>Ejercicio 3</p>
       <Contador/>
       <br/>
       <br/>
+      <p>Ejercicio 4</p>
       <Modal/>
     </div>
   );
